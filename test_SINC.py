53893a89c9048d7c110c4c37a295ca21919af774
YueWangMathbio <92970706+YueWangMathbio@users.noreply.github.com>
2025-07-11 21:32:33 -0700

    Add files via upload

--- test_SINC.py
+++ test_SINC.py
@@ -18,31 +18,33 @@ total_x_data = np.load(filename)
 filename = 'SINC/SINC_A.npy' # true GRN
 total_A = np.load(filename)
 
-group_count = 1000
+group_count = 8000
 res = np.zeros((3, 2))
 for i in range(1000):
-    A = total_A[i]
-    K_data = total_K_data[i]
-    x_data = total_x_data[i]
-    grn = Qwendy_Kx(K_data, x_data)
-    auroc, auprc = auroc_auprc(A, grn)
-    res[0, 0] += auroc / group_count
-    res[0, 1] += auprc / group_count
-    grn = Leqwendy_Kx(K_data, x_data)
-    auroc, auprc = auroc_auprc(A, grn)
-    res[1, 0] += auroc / group_count
-    res[1, 1] += auprc / group_count
-    grn = Teqwendy_Kx(K_data, x_data)
-    auroc, auprc = auroc_auprc(A, grn)
-    res[2, 0] += auroc / group_count
-    res[2, 1] += auprc / group_count
+    for j in range(8):
+        A = total_A[i]
+        K_data = total_K_data[i, j:j+4]
+        x_data = total_x_data[i, j:j+4]
+        grn = Qwendy_Kx(K_data, x_data)
+        auroc, auprc = auroc_auprc(A, grn)
+        res[0, 0] += auroc / group_count
+        res[0, 1] += auprc / group_count
+        grn = Leqwendy_Kx(K_data, x_data)
+        auroc, auprc = auroc_auprc(A, grn)
+        res[1, 0] += auroc / group_count
+        res[1, 1] += auprc / group_count
+        grn = Teqwendy_Kx(K_data, x_data)
+        auroc, auprc = auroc_auprc(A, grn)
+        res[2, 0] += auroc / group_count
+        res[2, 1] += auprc / group_count
+        
 res = np.round(res, 4)
 
 print(res)
 """
 rows: QWENDY, LEQWENDY, TEQWENDY
 columns: AUROC, AUPRC
-[[0.5314 0.572 ]
- [0.4972 0.5185]
- [0.9587 0.9142]]
+[[0.5107 0.5537]
+ [0.4990  0.5183]
+ [0.5932 0.6014]]
 """
